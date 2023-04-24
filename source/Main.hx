ec856889a621bcd6838be6791ae13aa91a909bc7
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-24 11:24:55 +0300

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -69,8 +69,6 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
-		#end
 	}
 }
